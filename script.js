66ead9e2d92e3763bad1a361b72296dda8376dab
Vesqum <marcin.pelszyk30@gmail.com>
2022-02-21 21:38:22 +0100

    finaly quote generate

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
-const quoteContaine = document.getElementById('quote-container');
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// Show New Quote
-function newQoute() {
-	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-
-	if (!quote.author) {
-		authorText.textContent = 'Unkonow';
-	} else {
-		authorText.textContent = quote.author;
-	}
-
-	// check Quote length to determine styling
-	if (quote.text.length < 50) {
-		quoteText.classList.add('long-quote');
-	} else {
-		quoteText.classList.remove('long-quote');
-	}
-
-	quoteText.textContent = quote.text;
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-// Get Quotes From Api
-async function getQuote() {
-	const apiUrl = 'https://type.fit/api/quotes';
+// Remove Loading Spinner
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
-	try {
-		const response = await fetch(apiUrl);
-		apiQuotes = await response.json();
-		newQoute();
-	} catch (error) {
-		alert(error);
-	}
+// Show New Quote
+function newQuote() {
+  loading();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  loading();
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    alert(error)
+  }
 }
 
 // Tweet Quote
 function tweetQuote() {
-	const urlTwitter = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(urlTwitter, '_blank');
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, '_blank');
 }
 
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
-
-
-newQuoteBtn.addEventListener('click', getQuote);
-twitterBtn.addEventListener('click', tweetQuote)
+// On Load
+getQuotes();
